ba2b8475c721d5d33ad06a32c7c0db67c2c92913
Vigneshprabu <155611674+VIKYPRU@users.noreply.github.com>
2024-05-03 19:09:06 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Clubs from './components/Clubs';
+import Events from './components/Events';
+import ContactUs from './components/ContactUs';
+import AddClub from './components/AddClub';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/services' element={<Services/>}/>
+      <Route path='/clubs' element={<Clubs/>}/>
+      <Route path='/events' element={<Events/>}/>
+      <Route path='/contactus' element={<ContactUs/>}/>
+      <Route path='/addclub'element={<AddClub/>}/>
+      </Routes>
+      
+     
+    </div>
+  );
+}
+
+export default App;
